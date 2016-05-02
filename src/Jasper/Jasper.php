ff8f0a1adbfe390592db55e471989acfc3ec8551
Christopher Mota <christopher.mota@ifnmg.edu.br>
2016-05-02 19:56:22 -0300

    Ajusts

--- src/Jasper/Jasper.php
+++ src/Jasper/Jasper.php
@@ -4,6 +4,8 @@ namespace chrmorandi\jasper;
 
 use yii\base\Component;
 use yii\db\Connection;
+use yii\db\Exception;
+use yii\helpers\BaseStringHelper;
 
 /**
  * Jasper implements JasperReport application component creating reports.
@@ -14,6 +16,8 @@ use yii\db\Connection;
  * ```php
  * 'jasper' => [
  *     'class' => 'chrmorandi\jasper',
+ *     'redirect_output' => false, //optional
+ *     'resource_directory' => false, //optional
  *     'db' => [
  *         'host' => localhost,
  *         'port' => 5432,    
@@ -24,7 +28,6 @@ use yii\db\Connection;
  *         //'jdbcDir' => './jdbc', **Defaults to ./jdbc
  *         //'jdbcUrl' => 'jdbc:postgresql://"+host+":"+port+"/"+dbname',
  *     ]
- *     
  * ]
  * ```
  *
@@ -35,38 +38,44 @@ class Jasper extends Component
 {
     /**
      * @var Connection|array|string the DB connection object or the application component ID of the DB connection.
-     * After the Jasper object is created, if you want to change this property, you should only assign it
-     * with a DB connection object.
+     *                              After the Jasper object is created, if you want to change this property, you should
+     *                              only assign it with a DB connection object.
      */
-    public $db;    
+    public $db;
+
+    /**
+     * @var bool|string contains path to report resource dir. If false given the input_file directory is used.
+     */
+    public $resource_directory = false;
+
+    /**
+     * @var bool redirect output and errors to /dev/null
+     */
     public $redirect_output = true;
-    protected $resource_directory = false; // Path to report resource dir or jar file
-    
-    protected $executable = "/../JasperStarter/bin/jasperstarter";
-    protected $the_command;    
+    public $output_file = false;
+
+    protected $executable = '/../JasperStarter/bin/jasperstarter';
+    protected $the_command;
     protected $background;
     protected $windows = false;
     protected $formats = ['pdf', 'rtf', 'xls', 'xlsx', 'docx', 'odt', 'ods', 'pptx', 'csv', 'html', 'xhtml', 'xml', 'jrprint'];
-    
 
     /**
      * Initializes the Jasper component.
-     * This method will initialize the [[db]] property to make sure it refers to a valid DB connection.
-     * @throws InvalidConfigException if [[db]] is invalid.
+     *
+     * @throws Exception if [[resource_directory]] not exist.
      */
-    function init()
+    public function init()
     {
         parent::init();
-                
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN'){
-           $this->windows = true;
+
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $this->windows = true;
         }
 
-        if (!$this->resource_directory) {
-            $this->resource_directory = __DIR__ . "/../../../../../";
-        } else {
-            if (!file_exists($this->resource_directory)){
-                throw new \Exception("Invalid resource directory", 1);
+        if ($this->resource_directory) {
+            if (!file_exists($this->resource_directory)) {
+                throw new Exception('Invalid resource directory', 1);
             }
         }
     }
@@ -74,31 +83,31 @@ class Jasper extends Component
     /**
      * Compile JasperReport template(JRXML) to native binary format, called Jasper file.
      * 
-     * @param string $input_file 
+     * @param string $input_file
      * @param string $output_file
      * @param string $output_file
-     * @param boolean $background
+     * @param bool   $background
      * 
      * @return
      */
     public function compile($input_file, $output_file = false, $background = false)
     {
-        if(is_null($input_file) || empty($input_file)){
-            throw new \Exception("No input file", 1);
+        if (is_null($input_file) || empty($input_file)) {
+            throw new Exception('No input file', 1);
         }
 
-        $command = __DIR__ . $this->executable;
+        $command = __DIR__.$this->executable;
 
-        $command .= " compile ";
+        $command .= ' compile ';
 
         $command .= $input_file;
 
-        if( $output_file !== false ){
-            $command .= " -o " . $output_file;
+        if ($output_file !== false) {
+            $command .= ' -o '.$output_file;
         }
 
-        $this->background       = $background;
-        $this->the_command      = escapeshellcmd($command);
+        $this->background = $background;
+        $this->the_command = escapeshellcmd($command);
 
         return $this;
     }
@@ -107,111 +116,112 @@ class Jasper extends Component
      * Generates report . Accepts files in the format ".jrxml" or ".jasper".
      * 
      * ```php
-     * 
      * $jasper->process(
      *     __DIR__ . '/vendor/chrmorandi/yii2-jasper/examples/hello_world.jasper',
-     *     false,
+     *     ['php_version' => 'xxx']
      *     ['pdf', 'ods'],
-     *     array("php_version" => "xxx")
      * )->execute();
      * ```
      * 
-     * @param string $input_file 
-     * @param string $output_file
-     * @param array $format available formats : pdf, rtf, xls, xlsx, docx, odt, ods, pptx, csv, html, xhtml, xml, jrprint.
-     * @param array $parameters
-     * @param boolean $background
+     * @param string $input_file
+     * @param array  $parameters
+     * @param array  $format      available formats : pdf, rtf, xls, xlsx, docx, odt, ods, pptx, csv, html, xhtml, xml, jrprint.
+     * @param string $output_file if false the input_file directory is used. Default is false
+     * @param bool   $background  if true report is runing in the backgrount. The return status is 0. Default is false
      */
-    public function process($input_file, $output_file = false, $format = ["pdf"], $parameters = [],$background = false)
+    public function process($input_file, $parameters = [], $format = ['pdf'], $output_file = false, $background = false)
     {
-        if(is_null($input_file) || empty($input_file)){
-            throw new \Exception("No input file", 1);
+        if (is_null($input_file) || empty($input_file)) {
+            throw new Exception('No input file', 1);
         }
 
-        if( is_array($format) )
-        {
-            foreach ($format as $key)
-            {
-                if( !in_array($key, $this->formats))
-                    throw new \Exception("Invalid format!", 1);
+        if (is_array($format)) {
+            foreach ($format as $key) {
+                if (!in_array($key, $this->formats)) {
+                    throw new Exception('Invalid format!', 1);
+                }
             }
         } else {
-            if( !in_array($format, $this->formats))
-                    throw new \Exception("Invalid format!", 1);
+            if (!in_array($format, $this->formats)) {
+                throw new Exception('Invalid format!', 1);
+            }
         }
 
-        $command = __DIR__ . $this->executable;
+        $command = __DIR__.$this->executable;
 
-        $command .= " process ";
+        $command .= ' process ';
 
         $command .= $input_file;
 
-        if( $output_file !== false ){
-            $command .= " -o " . $output_file;
+        if ($output_file !== false) {
+            $command .= ' -o '.$output_file;
         }
 
-        if( is_array($format) ){
-            $command .= " -f " . join(" ", $format);
-        }else{
-            $command .= " -f " . $format;
+        if (is_array($format)) {
+            $command .= ' -f '.implode(' ', $format);
+        } else {
+            $command .= ' -f '.$format;
         }
 
-        // Resources dir
-        $command .= " -r " . $this->resource_directory;
-
-        if( count($parameters) > 0 )
-        {
-            $command .= " -P";
-            foreach ($parameters as $key => $value)
-            {
-                $command .= " " . $key . "=" . $value;
-            }
+        if ($this->resource_directory) {
+            $command .= ' -r '.$this->resource_directory;
+        }
+        else {
+            $resource = BaseStringHelper::dirname($input_file);
+            $command .= ' -r '.$this->resource_directory;
         }
 
-        if(isset($this->db) )
-        {
-            $command .= " -t " . $this->db['driver'];
-            $command .= " -u " . $this->db['username'];
-
-            if( isset($this->db['password']) && !empty($this->db['password']) ){
-                $command .= " -p " . $this->db['password'];
-            }
-
-            if( isset($this->db['host']) && !empty($this->db['host']) ){
-                $command .= " -H " . $this->db['host'];
-            }
-
-            if( isset($this->db['dbname']) && !empty($this->db['dbname']) ){
-                $command .= " -n " . $this->db['dbname'];
-            }
-
-            if( isset($this->db['port']) && !empty($this->db['port']) ){
-                $command .= " --db-port " . $this->db['port'];
-            }
-
-            if( isset($this->db['jdbc_url']) && !empty($this->db['jdbc_url']) ){
-                $command .= " --db-url " . $this->db['jdbc_url'];
-            }
-
-            if ( isset($this->db['jdbc_dir']) && !empty($this->db['jdbc_dir']) ){ 
-                $command .= ' --jdbc-dir ' . $this->db['jdbc_dir'];
+        if (count($parameters) > 0) {
+            $command .= ' -P';
+            foreach ($parameters as $key => $value) {
+                $command .= ' '.$key.'='.$value;
             }
         }
 
-        $this->background       = $background;
-        $this->the_command      = escapeshellcmd($command);
+        if (isset($this->db)) {
+            $command .= ' -t '.$this->db['driver'];
+            $command .= ' -u '.$this->db['username'];
+
+            if (isset($this->db['password']) && !empty($this->db['password'])) {
+                $command .= ' -p '.$this->db['password'];
+            }
+
+            if (isset($this->db['host']) && !empty($this->db['host'])) {
+                $command .= ' -H '.$this->db['host'];
+            }
+
+            if (isset($this->db['dbname']) && !empty($this->db['dbname'])) {
+                $command .= ' -n '.$this->db['dbname'];
+            }
+
+            if (isset($this->db['port']) && !empty($this->db['port'])) {
+                $command .= ' --db-port '.$this->db['port'];
+            }
+
+            if (isset($this->db['jdbc_url']) && !empty($this->db['jdbc_url'])) {
+                $command .= ' --db-url '.$this->db['jdbc_url'];
+            }
+
+            if (isset($this->db['jdbc_dir']) && !empty($this->db['jdbc_dir'])) {
+                $command .= ' --jdbc-dir '.$this->db['jdbc_dir'];
+            }
+        }
+
+        $this->background = $background;
+        $this->the_command = escapeshellcmd($command);
 
         return $this;
     }
 
     public function list_parameters($input_file)
     {
-        if(is_null($input_file) || empty($input_file))
-            throw new \Exception("No input file", 1);
+        if (is_null($input_file) || empty($input_file)) {
+            throw new Exception('No input file', 1);
+        }
 
-        $command = __DIR__ . $this->executable;
+        $command = __DIR__.$this->executable;
 
-        $command .= " list_parameters ";
+        $command .= ' list_parameters ';
 
         $command .= $input_file;
 
@@ -220,29 +230,28 @@ class Jasper extends Component
         return $this;
     }
 
-    public function output()
-    {
-        return escapeshellcmd($this->the_command);
-    }
-
     public function execute($run_as_user = false)
     {
-        if( $this->redirect_output && !$this->windows)
-            $this->the_command .= " > /dev/null 2>&1";
+        if ($this->redirect_output && !$this->windows) {
+            $this->the_command .= ' > /dev/null 2>&1';
+        }
 
-        if( $this->background && !$this->windows )
-            $this->the_command .= " &";
+        if ($this->background && !$this->windows) {
+            $this->the_command .= ' &';
+        }
 
-        if( $run_as_user !== false && strlen($run_as_user > 0) && !$this->windows )
-            $this->the_command = "su -u " . $run_as_user . " -c \"" . $this->the_command . "\"";
+        if ($run_as_user !== false && strlen($run_as_user > 0) && !$this->windows) {
+            $this->the_command = 'su -u '.$run_as_user.' -c "'.$this->the_command.'"';
+        }
 
-        $output     = [];
+        $output = [];
         $return_var = 0;
 
         exec($this->the_command, $output, $return_var);
 
-        if($return_var != 0)
-            throw new \Exception("Your report has an error and couldn't be processed! Try to output the command using the function `output();` and run it manually in the console.", 1);
+        if ($return_var != 0) {
+            throw new Exception('Your report has an error and couldn\'t be processed! Try to output the command: ' . escapeshellcmd($this->the_command), 1);
+        }
 
         return $output;
     }
